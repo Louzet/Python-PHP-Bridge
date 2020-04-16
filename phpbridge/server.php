2b95015bdc8e7943fa46ecb8fab197f717bdfeba
Mickael Louzet <39524830+Louzet@users.noreply.github.com>
2020-04-17 00:52:58 +0200

    Update phpbridge/server.php
    i'm ok with this change, that's thr right path to the autoload
    
    Co-Authored-By: Jan Verbeek <jan.verbeek@posteo.nl>

--- phpbridge/server.php
+++ phpbridge/server.php
@@ -10,8 +10,8 @@
 
 declare(strict_types=1);
 
-if (file_exists('../vendor/autoload.php')) {
-    require_once '../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../../../../vendor/autoload.php')) {
+    require_once __DIR__ . '/../../../../vendor/autoload.php';
 } else {
     // Adapted from the PHP-FIG example autoloader
     spl_autoload_register(function ($class) {
